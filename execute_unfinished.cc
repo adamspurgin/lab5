0c9e1b3300b104947be02e2a9bd51db682c6b700
Adam Spurgin <adam.spurgin@gmail.com>
2011-05-24 16:40:22 -0700

    update

--- execute_unfinished.cc
+++ execute_unfinished.cc
@@ -7,8 +7,6 @@ unsigned int signExtend16to32ui(short i) {
   return static_cast<unsigned int>(static_cast<int>(i));
 }
 
-	//this is a new comment work, dammit
-
 void execute() {
   Data32 instr = imem[pc];
   GenericType rg(instr);
@@ -21,15 +19,19 @@ void execute() {
   pc = pctarget;
   switch(rg.op) {
   case OP_SPECIAL:
-	switch(rg.func) {
+    switch(rg.func) {
     case SP_ADDU:
       rf.write(rt.rd, rf[rt.rs] + rf[rt.rt]);
       break;
     case SP_SLL:
       rf.write(rt.rd, rf[rt.rt] << rt.sa);
       break;
-    case SP_AND:
-	  rf.wr
+    case SP_SLT:
+	  rf.write(rt.rd, (rf[rt.rs]<rf[rt.rt])?1:0);
+	  break;
+	case SP_JR:
+	  pc.write(rt.rs);
+	  break;
     default:
       cout << "Unsupported instruction: ";
       instr.printI(instr);
@@ -37,76 +39,65 @@ void execute() {
       break;
     }
     break;
-  case OP_ANDI:   //not sure if works
-	 rf.write(ri.rt, rf[ri.rs] & ri.imm);
-	 break;
   case OP_ADDIU:
     rf.write(ri.rt, rf[ri.rs] + signExtend16to32ui(ri.imm));
     break;
-  case OP_ADDI:
-    rf.write(ri.rt, rf[ri.rs] + ri.imm);
-    break;
   case OP_ORI:
 	rf.write(ri.rt, rf[ri.rs] | ri.imm);
+	break;
+  case OP_SLTI:
+	rf.write(ri.rt, (ri.rs<ri.imm)?1:0);
+	break;
+  case OP_SLTIU:			//not sure about this one
+	rf.write(ri.rt, (ri.rs<signExtend16to32ui(ri.imm))?1:0);
 	break;
   case OP_SW:
     addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
     caches.access(addr);
     dmem.write(addr, rf[ri.rt]);
     break;
-  case OP_SB:  //not sure if this works
-	addr = rf[ri.rt] + signExtend16to32ui(ri.imm);
+  case OP_SB:					//still not sure about this one
+	addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
 	caches.access(addr);
-	dmem.write(addr, rf[ri.rt]);
+	dmem.write(addr, rf[ri.rt].data_ubyte4);
 	break;
-  case OP_LBU:
-	break;
+  case OP_LUI:
+	addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
+	caches.access(addr);
+	rf.write(ri.rt, dmem[addr].data_uint());
   case OP_LW:
     addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
     caches.access(addr);
     rf.write(ri.rt, dmem[addr]);
     break;
-  case OP_SW:   //not sure if works
- 	addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
+  case OP_LB:					//still not sure about this one
+	addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
 	caches.access(addr);
-	rf.write(ri.rt, dmem[addr]);
+	rf.write(ri.rt, signExtend16to32ui(dmem[addr].data_ubyte4));
+  case OP_LBU:					//still not sure about this one
+	addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
+	caches.access(addr);
+	rf.write(ri.rt, dmem[addr].data_ubyte4);
 	break;
-  case: OP_LB:
-    addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
-    caches.access(addr);
-    rf.write(ri.rt, dmem[addr]);
-    break;
-  case: OP_SLTI:
-	int tempval = (ri.rs < ri.imm)?1:0;
-	rf.write(ri.rt, tempval);
+  case OP_BNE:					//still not sure about this one
+	if(rf[ri.rt] != rf[ri.rs])
+		pc.write(pc.d + signExtend16to32ui(ri.imm));
 	break;
-  case: SLTIU
-	int tempval = (ri.rs < signExtend16to32ui(ri.im))?1:0;
-	rf.write(ri.rt, tempval);
+  case OP_BEQ:					//still not sure about this one
+	if(rf[ri.rt] != 0)
+		pc.write(signExtend16to32ui(ri.imm));
 	break;
-  case: OP_BNE:
-	if(ri.rs != ri.rt)
-		pc += ri.imm;
+  case OP_BLEZ: 				//still not sure about this one
+	if(rf[ri.rt] <= 0)
+		pc.write(signExtend16to32ui(ri.imm));
 	break;
-  case: BEQ
-	if(ri.rs == ri.rt)
-		pc += ri.imm*4;
+  case OP_J:
+	pc.write(rj.target);
 	break;
-  case: BLEZ
-	if(ri.rs < 0)
-		pc += ri.imm*4;
+  case OP_JAL:
+	rf.write(31, pc.d);
+	pc.write(signExtend16to32ui(ri.imm));
 	break;
-  case: LUI
-	rf.write(ri.rt, ri.imm << 16);
-	break;
-  case: JAL
-  case: J
-	pc = rj.target;
-	break;
-  case: ADDU
-  case: SLT
-  case: JR
-  case: SLL
   default:
     cout << "Unsupported instruction: ";
     instr.printI(instr);
